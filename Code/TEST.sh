3f1679e092f401b93ef0f0cd0b2583605610f4c2
mmf1amt <78595179+mmf1amt@users.noreply.github.com>
2021-07-30 19:42:34 +0100

    Update TEST.sh

--- Code/TEST.sh
+++ Code/TEST.sh
@@ -1,2 +1,4 @@
+echo This is a test.
 whoami >> /var/log/UserIsLoggedInAs.txt 
 date >> /var/log/UserIsLoggedInAs.txt 
+#The above lines of code print the message "This is a test" to screen and log the user logged in and the a date stamp to the file /var/log/UserIsLoggedInAs.txt located on the local server. 
